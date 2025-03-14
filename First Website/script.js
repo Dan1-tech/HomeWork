70c7e3cd8a4e49b417336410a0a2b58de4cb1fe9
Dan1-tech <dan250897@icloud.com>
2025-03-14 22:21:26 +0200

    Hello Aroken.Ru Added

---
+++ First Website/script.js
@@ -0,0 +1,8 @@
+
+const button = document.querySelector('button');
+const image = document.getElementById('myImage');
+
+
+button.addEventListener('click', () => {
+  image.src = '/First Website/images/Dog.jpeg';
+});
