0e5f29cc0d4c5b023d65b251dec45bce4e13a7bb
Dan1-tech <dan250897@icloud.com>
2025-03-14 22:38:48 +0200

    Delete First Website directory

--- First Website/script.js
+++
@@ -1,8 +0,0 @@
-
-const button = document.querySelector('button');
-const image = document.getElementById('myImage');
-
-
-button.addEventListener('click', () => {
-  image.src = '/First Website/images/Dog.jpeg';
-});
